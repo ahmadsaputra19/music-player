4ee28f2c000e5d6eeb0ce28dd1c9b392e614c3b8
alasyiqin-pekalongan <alasyiqinpekalongan@gmail.com>
2024-02-23 13:56:46 +0700

    menambahkan file project musik

---
+++ data.js
@@ -0,0 +1,38 @@
+let songs = [
+  {
+    name: 'Banjari - Assalamualaik Busyrolana',
+    path: '/audio/habsyi/AA - Assalamu_alaik_Busyrolana.mp3',
+    artist: 'Al-asyiqin Pekalongan',
+    cover: 'images/cover1.png',
+  },
+  {
+    name: 'Banjari - Sholawatullahi Taghsya',
+    path: '/audio/habsyi/AA - Solawatullahi taghsya.mp3',
+    artist: 'Al-asyiqin Pekalongan',
+    cover: 'images/cover1.png',
+  },
+  {
+    name: 'Banjari - Tabarokallah',
+    path: '/audio/habsyi/AA - Tabarakallah.mp3',
+    artist: 'Al-asyiqin Pekalongan',
+    cover: 'images/cover1.png',
+  },
+  {
+    name: 'Banjari - Ya Mimnah Wal Madinah',
+    path: '/audio/habsyi/AA - Ya Mimnah Wal Madina.mp3',
+    artist: 'Al-asyiqin Pekalongan',
+    cover: 'images/cover1.png',
+  },
+  {
+    name: 'Feat Gus Miftah - Sholatullah Salamullah',
+    path: '/audio/habsyi/AA feat Gus Miftah - Sholatullah salamullah.mp3',
+    artist: 'Al-asyiqin Pekalongan',
+    cover: 'images/cover1.png',
+  },
+  {
+    name: 'Feat SQ - Habibi Ya Muhammad',
+    path: '/audio/habsyi/AA feat SQ - Habibi ya Muhammad.mp3',
+    artist: 'Al-asyiqin Pekalongan',
+    cover: 'images/cover1.png',
+  },
+];
