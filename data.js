cd44691bace2496bf29b83beba6a63b81f5e866b
alasyiqin-pekalongan <alasyiqinpekalongan@gmail.com>
2024-02-23 17:47:05 +0700

    menambahkan image

--- data.js
+++ data.js
@@ -1,38 +1,8 @@
 let songs = [
   {
-    name: 'Banjari - Assalamualaik Busyrolana',
-    path: '/audio/habsyi/AA - Assalamu_alaik_Busyrolana.mp3',
-    artist: 'Al-asyiqin Pekalongan',
-    cover: 'images/cover1.png',
-  },
-  {
-    name: 'Banjari - Sholawatullahi Taghsya',
-    path: '/audio/habsyi/AA - Solawatullahi taghsya.mp3',
-    artist: 'Al-asyiqin Pekalongan',
-    cover: 'images/cover1.png',
-  },
-  {
-    name: 'Banjari - Tabarokallah',
-    path: '/audio/habsyi/AA - Tabarakallah.mp3',
-    artist: 'Al-asyiqin Pekalongan',
-    cover: 'images/cover1.png',
-  },
-  {
-    name: 'Banjari - Ya Mimnah Wal Madinah',
-    path: '/audio/habsyi/AA - Ya Mimnah Wal Madina.mp3',
-    artist: 'Al-asyiqin Pekalongan',
-    cover: 'images/cover1.png',
-  },
-  {
-    name: 'Feat Gus Miftah - Sholatullah Salamullah',
-    path: '/audio/habsyi/AA feat Gus Miftah - Sholatullah salamullah.mp3',
-    artist: 'Al-asyiqin Pekalongan',
-    cover: 'images/cover1.png',
-  },
-  {
-    name: 'Feat SQ - Habibi Ya Muhammad',
-    path: '/audio/habsyi/AA feat SQ - Habibi ya Muhammad.mp3',
-    artist: 'Al-asyiqin Pekalongan',
+    name: 'Music Work Relax 1',
+    path: '/audio/programming/work-music1.mp3',
+    artist: 'Super Hero Mix',
     cover: 'images/cover1.png',
   },
 ];
