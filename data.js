077efedb4278422c96c285a92a708d20a0ef1c38
alasyiqin-pekalongan <alasyiqinpekalongan@gmail.com>
2024-02-27 09:43:43 +0700

    update data.js

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 let songs = [
   {
     name: 'Music Work Relax 1',
-    path: 'audio/programming/work-music1.mp3',
+    path: 'work-music1.mp3',
     artist: 'Super Hero Mix',
-    cover: 'img/music1.jpg',
+    cover: 'music1.jpg',
   },
   {
     name: 'Music Chill Beat',
-    path: 'audio/programming/music-chill-beats-relax.mp3',
+    path: 'music-chill-beats-relax.mp3',
     artist: 'Super Hero Mix',
-    cover: 'img/music1.jpg',
+    cover: 'music1.jpg',
   }
 ];
