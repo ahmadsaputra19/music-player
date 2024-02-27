dd4a1458de971aadf71b61e327fc4bb5f203e3cb
alasyiqin-pekalongan <alasyiqinpekalongan@gmail.com>
2024-02-27 09:35:20 +0700

    memperbarui app.js

--- data.js
+++ data.js
@@ -1,14 +1,14 @@
 let songs = [
   {
     name: 'Music Work Relax 1',
-    path: '/audio/programming/work-music1.mp3',
+    path: 'audio/programming/work-music1.mp3',
     artist: 'Super Hero Mix',
-    cover: '/img/music1.jpg',
+    cover: 'img/music1.jpg',
   },
   {
     name: 'Music Chill Beat',
-    path: '/audio/programming/music-chill-beats-relax.mp3',
+    path: 'audio/programming/music-chill-beats-relax.mp3',
     artist: 'Super Hero Mix',
-    cover: '/img/music1.jpg',
+    cover: 'img/music1.jpg',
   }
 ];
